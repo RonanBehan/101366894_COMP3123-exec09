cd297cca945734be80ace5ca46e4c1bdfdf0d368
RonanBehan <144288415+RonanBehan@users.noreply.github.com>
2024-10-31 21:12:09 -0400

    Add files via upload

---
+++ layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to Fullstack Development 1</h1>
+      <h3>React JS Programming Week09 Exercise</h3>
+      <p>Ronan Behan</p>
+      <p>101366894</p>
+      <p>George Brown College, Toronto</p>
+    </div>
+  );
+}
+
+export default App;
